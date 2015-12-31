be3466de59f492b4258998f2bdacedc6f944095c
Nicolás <nicokiva@hotmail.com>
2015-12-31 17:51:44 -0300

    improved url beautifier

--- back/core/main.php
+++ back/core/main.php
@@ -3,8 +3,10 @@
 	include_once('class/application.php');
 	include_once('class/controller.php');
 
+	$params = array_merge ($_GET, $_POST);
+	var_dump($params);
 
-	$request = new request($_GET['controller'], $_GET['action'], $_POST);
+	$request = new request($_GET['controller'], $_GET['action'], $params);
 	$app = new application();
 	$app->run($request);
 ?>
