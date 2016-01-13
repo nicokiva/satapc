ad4d44b71bc37f9a019420df421c8ce15a369d15
Nicolás <nicokiva@hotmail.com>
2016-01-12 22:37:35 -0300

    added meta utf8

--- front/view/general/header.php
+++ front/view/general/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="UTF-8">
+
 		<link rel="stylesheet" type="text/css" href="<?= $resourcesLoader->resolvePath('WEB_RESOURCE_CSS', 'main.css'); ?>" />
 
 		<script type="text/javascript" src="<?= $resourcesLoader->resolvePath('WEB_RESOURCE_JS', 'jquery-2.1.4.min.js'); ?>"></script>
