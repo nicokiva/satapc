850f1adaa75db0dace4246ef5c4e9dff0bedaf94
Nicolás <nicokiva@hotmail.com>
2016-01-17 18:10:00 -0300

    added favicon

--- front/view/general/header.php
+++ front/view/general/header.php
@@ -3,6 +3,8 @@
 	<head>
 		<meta charset="UTF-8">
 
+		<link rel="icon" type="image/png" href="<?= $resourcesLoader->resolvePath('WEB_RESOURCE_IMG', 'icons/s_key.png'); ?>">
+
 		<link rel="stylesheet" type="text/css" href="<?= $resourcesLoader->resolvePath('WEB_RESOURCE_CSS', 'main.css'); ?>" />
 
 		<script type="text/javascript" src="<?= $resourcesLoader->resolvePath('WEB_RESOURCE_JS', 'jquery-2.1.4.min.js'); ?>"></script>
